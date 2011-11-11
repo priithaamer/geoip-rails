cae85b1fb169a4c2c2cfe18e7297281ae6389c1a
Priit Haamer <priit@fraktal.ee>
2011-11-11 12:11:59 +0200

    Validate IP-address before running SQL query.
    Invalid IP-address caused MySQL to drop connection. Fixes #3.

--- app/models/geoip/location.rb
+++ app/models/geoip/location.rb
@@ -1,9 +1,13 @@
 module Geoip
   class Location < Geoip::Base
     
+    IPADDR_REGEX = /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
+    
     has_many :blocks
     
     def self.find_by_ip(ip, &block)
+      return nil unless IPADDR_REGEX.match(ip)
+      
       location = self.joins(:blocks).where('MBRCONTAINS(ip_poly, POINTFROMWKB(POINT(INET_ATON(?), 0)))', ip).first
       
       if location and block_given?
